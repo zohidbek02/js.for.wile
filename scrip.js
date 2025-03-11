ed034e36fa18f0a6f4c9b90e0acdaf6032a4a3c9
zohidbek02 <zohidokengo2o4@gmail.com>
2025-03-12 00:41:56 +0300

    for commit

--- scrip.js
+++ scrip.js
@@ -86,6 +86,8 @@
     // } 
     // alert(n);
 
+    // foydalanuvchi musbat son kirtishni sorang 
+    // agar manifi son kirtsa yana takroran kirtsin 
 
     // let n;
     // do{
@@ -97,19 +99,102 @@
     // alert(n);
 
 
+    // 1 dan 20gava bolgan ekranga chiqaring
+    // lekin 13 ga keganda sindring
 
 
-
-
-    // let a = parseInt(prompt("raqam kirting"))
-    // switch (a % 2 == 1) {
-    //     case "1":
-            
+    // for( let i = 1; i <= 20; i++){
+    //     console.log(i);
+    //     if(i == 13){
     //         break;
-    
-    //     default:
-    //         break;
+    //     }
+        
     // }
 
 
+    // For1. a va b butun sonlari berilgan (a < b). a va b sonlari orasidagi barcha butun sonlarni (a va b ni ham) chiqaruvchi va chiqarilgan sonlar sonini chiqaruvchi programma tuzilsin. (a va b xam chiqarilsin).
+
+    // let a = +prompt("son_1 kirting");
+    // let b = +prompt("son_2 kirting");
+
+    // for(let i = a; i <= b; i++){
+    //     console.log(i);
+        
+    // }
+
+    // console.log(`${a} vs ${b} orasida ${b - a + 1} ta son bor `);
+    
+
+
+    
+// For2. a va b butun sonlari berilgan (a < b). a va b sonlari orasidagi barcha butun sonlarni (a va b dan tashqari) kamayish tartibida chiqaruvchi va chiqarilgan sonlar sonini chiqaruvchi progma tuzilsin.
+
+
+    // let a = +prompt("kichik sondi kirting");
+    // let b = +prompt("katta sondi kiriting");
+    
+    //   for(let i = b - 1; i > a; i--){
+        //     console.log(i);
+    //   }
+    //   console.log(`${b} vs ${a} orasida ${b - a - 1} ta son bor`);
+      
+
+    // For3. Bir kg konfetning narxi berilgan (haqiqiy son). 1, 2, 10 kg konfetni narxini chiqaruvchi programma tuzilsin.
+
+
+    // let a = +prompt("narxdi kirting");
+
+
+    // for(let i = 1; i <= 10; i++){
+
+    //     console.log(`${i * a} som`);
+    // }
+
+    // For4. Bir kg konfetning narxi berilgan (haqiqiy son). 1.2, 1.4, ..., 2 kg konfetni narxini chiqaruvchi programma tuzilsin.
+
+    // let a = parseFloat(prompt("narxni kirting"));
+    
+    
+    // for(let i = 1.2; i <= 2; i += 0.2){
+    //     console.log(`${i * a}`);
+        
+    // }
+
+
+    // For5. a va b butun sonlari berilgan (a < b). a dan b gacha bo'lgan barcha butun sonlar yig'indisini chiqaruvchi programma tuzilsin.
+
+    // let a = +prompt("son1 kirting");
+    // let b = +prompt("son2 kirting");
+    // let sum = 0;
+    // for(let i = a; i < b; i++){
+    //     sum += i 
+        
+        
+    // }
+
+    // console.log(sum);
+
+    
+    // While1. A va B butun musbat sonlari berilgan (A> B). A usunlikdagi kesmada maksimal darajada B kesma joylashtirilgan. A kesmaning bo'sh qismini aniqlovchi programma tuzilsin. Ko'paytirish va bo'lish amallarini ishlatmang.
+
+
+    // let a = +prompt("A kesmani kirting");
+    // let b = +prompt("B kesmani kirting ")
+
+    // while(a >= b ){
+    //     a -= b    
+    // }
+    // console.log(a)
+
+    // While2. A va B butun musbat sonlari berilgan (A > B). A usunlikdagi kesmada B kesmadan nechta joylashtirish mumkinligini aniqlovchi programma tuzilsin. Ko'paytirish va bo'lish amallarini ishlatmang.
+
+
+    //  let a = +prompt("A kesmani kirting");
+    // let b = +prompt("B kesmani kirting ");
+    // let count = 0;
+    // while(a >= b){
+    //     a -= b
+    //     count++        
+    // }
+    // console.log(count);
     
