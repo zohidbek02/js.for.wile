b96cae065f21bfff44a9f7cf98a443f61e51a7ce
zohidbek02 <zohidokengo2o4@gmail.com>
2025-03-11 22:14:40 +0300

    first commit

---
+++ scrip.js
@@ -0,0 +1,115 @@
+    // 0 dan n gacha bolgan son
+
+        // for(i= 0; i < 5 ; i++){
+        //     console.log(i)
+        // }
+
+        // n dan 1 gacha bolgan son
+
+        // for( a = 10; a > 0; a--){
+        //     console.log(a)
+        // }
+
+        // task 3
+        // let sum = 0;
+        // for(i = 0; i <= 10; i++){
+        //         console.log(sum +=i)
+        // }
+
+        // task 4
+        // let sum = 0;
+        // for(i = 0; i < 10; i++){
+        //     if(i % 2 == 0){
+        //         console.log(sum+=i)
+        //     }
+        // }
+
+        // task 5
+
+        // let sum = 0;
+        // for (i = 0; i < 10; i++) {
+        //     if (i % 2 == 1) {
+        //         sum += i
+        //         console.log(i);
+        //     }
+        // }
+        // console.log(sum);
+
+
+        // task 6
+
+        // let sum = prompt("son kirting");
+        // for( let i = 0; i < sum; i++){
+        //     if(sum % i == 0){
+        //         console.log(i)
+        //     }
+        // }
+        // console.log(sum)
+
+        // task 7
+
+        // let sum = parseInt(prompt("son kirting"));
+        // let total = 0
+        // for (let i = 0; i <= sum; i++) {
+        //     if (sum % i == 0) {
+        //         total += i
+        //         console.log(i);
+        //     }
+        // }
+        // console.log("yig'indi",total);
+
+
+        // task 8
+
+
+    //     let n = parseInt(prompt("raqam kirting"));
+    //     let chek = true
+    //     for(let i = 2; i  <= Math.sqrt(n); i++ ){
+    //        if(n % i == 0){
+    //            console.log("tub emas");
+    //            chek = false
+    //            break
+    //        }
+    // }
+    // if(chek){
+    //     console.log("tub son");
+    // }
+
+
+    // qoshmcha
+
+    // task 1
+
+    // let n = parseInt(prompt("musbat sondi kirting"));
+    // while (n < 0) {
+    //     n = parseInt(prompt("musbat sondi kirting"));
+    // } 
+    // alert(n);
+
+
+    // let n;
+    // do{
+    //    n = parseInt(prompt("musbat sondi kirting"));
+
+    // }while(n < 0 || isNaN(n))
+
+
+    // alert(n);
+
+
+
+
+
+
+    // let a = parseInt(prompt("raqam kirting"))
+    // switch (a % 2 == 1) {
+    //     case "1":
+            
+    //         break;
+    
+    //     default:
+    //         break;
+    // }
+
+
+    
